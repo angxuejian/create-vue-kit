68fa99a65ac30f08d509b5dba0568b933e5e8233
angxuejian <xuejian.ang@gmail.com>
2025-11-05 17:24:28 +0800

    feat: version 1.0.1

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,7 @@ const prompts = require("prompts");
 async function run() {
   try {
     const cwdName = path.basename(process.cwd());
-    const defaultPath = "./test122";
+    const defaultPath = "./" + cwdName;
 
     console.log("\n");
 
